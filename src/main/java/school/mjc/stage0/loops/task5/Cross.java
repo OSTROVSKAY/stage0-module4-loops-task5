3863a7d7c076306a2dfcf070c3ae64ab347f4e67
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-11-27 13:22:27 +0300

    Finish : Module #4.2. Task #5. Nested Loops

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -3,5 +3,29 @@ package school.mjc.stage0.loops.task5;
 public class Cross {
     public void printCross(int sideLength) {
 
+        for(int i = 1; i <= sideLength; i++) {
+
+            for(int j = 1; j <= sideLength; j++) {
+
+                if( i == ( ( (sideLength - 1) / 2 ) + 1 ) ) {
+
+                    System.out.print("8");
+
+                } else if (j == ( ( (sideLength - 1) / 2 ) + 1 )) {
+
+                    System.out.print("8");
+
+                } else if ( j != ( ( (sideLength - 1) / 2 ) + 1 ) ) {
+
+                    System.out.print(" ");
+
+                }
+
+            }
+
+            System.out.println("");
+
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -3,5 +3,79 @@ package school.mjc.stage0.loops.task5;
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
 
+        if (height % 2 != 0) {
+
+            int half = ((height - 1) / 2) + 1;
+
+            for (int i = 1; i <= height; i++) {
+
+                for (int j = 1; j <= height; j++) {
+
+                    if (i < half && (j >= i && j <= (height - i + 1))) {
+
+                        System.out.print("8");
+
+                    } else if (i < half && (j < i || j > (height - i + 1))) {
+
+                        System.out.print(" ");
+
+                    } else if (i == half && i == j) {
+
+                        System.out.print("8");
+
+                    } else if (i == half && i != j) {
+
+                        System.out.print(" ");
+
+                    } else if (i > half && j > (height - i) && j <= i) {
+
+                        System.out.print("8");
+
+                    } else if (i > half && (j <= (height - i) || j > i)) {
+
+                        System.out.print(" ");
+
+                    }
+
+                }
+
+                System.out.println("");
+
+            }
+
+        } else {
+
+            int half = height/2;
+
+            for(int i = 1; i <= height; i++) {
+
+                for(int j = 1; j <= height; j++) {
+
+                    if(i <= half && j >= i && j <= height - i + 1 ) {
+
+                        System.out.print("8");
+
+                    } else if(i <= half && (j < i || j > height - i + 1) ) {
+
+                        System.out.print(" ");
+
+                    } else if( i > half && j > (height - i) && j <= i ) {
+
+                        System.out.print("8");
+
+                    } else if ( i > half && (j > (height - i) || j <= i )) {
+
+                        System.out.print(" ");
+
+                    }
+
+                }
+
+                System.out.println("");
+
+            }
+
+        }
+
     }
 }
